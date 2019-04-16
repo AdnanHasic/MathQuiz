41ec3c521d5ff544a2e7b28793b4007dce21a5a8
adnan hasic <hasic.adnan.ado@gmail.com>
2019-04-16 22:11:14 +0200

    Napravio metodu za oduzimanje

--- src/MathematicalOperations.java
+++ src/MathematicalOperations.java
@@ -14,7 +14,32 @@ public class MathematicalOperations {
 	    
 	    int rezultat = MathQuiz.unos.nextInt();
 	    
-	    if (rezultat == (x +y)) {
+	    if (rezultat == (x + y)) {
+	    	
+	    	System.out.println("Vas odgovor je tacan");
+	    	break;
+	    	
+	    } else {
+	    	
+	    	System.out.println("Vas odgovor je pogresan, pokusajte ponovo");
+	    }
+	    
+		}
+		
+	}
+	
+	public void oduzimanjeBrojeva() {
+
+		int x = (int)(Math.random()*10+1);
+		int y = (int)(Math.random()*10+1);
+	
+		for ( ; ; ) {
+			
+	    System.out.println(x  + " - " + y + " = ");
+	    
+	    int rezultat = MathQuiz.unos.nextInt();
+	    
+	    if (rezultat == (x - y)) {
 	    	
 	    	System.out.println("Vas odgovor je tacan");
 	    	break;

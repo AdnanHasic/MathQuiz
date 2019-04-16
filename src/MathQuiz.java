41b561c51c8289a3bb357783e179aced38a9c461
adnan hasic <hasic.adnan.ado@gmail.com>
2019-04-16 22:09:14 +0200

    Napravio metodu za sabiranje

--- src/MathQuiz.java
+++ src/MathQuiz.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 
 public class MathQuiz {
+	
+	public static Scanner unos = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		MathematicalOperations mathOp = new MathematicalOperations();
+		
+		mathOp.sabiranjeBrojeva();
 
 	}
 
--- src/MathematicalOperations.java
+++ src/MathematicalOperations.java
@@ -1,5 +1,38 @@
 
 public class MathematicalOperations {
 	
+	
+	
+	public void sabiranjeBrojeva() {
+
+		int x = (int)(Math.random()*10+1);
+		int y = (int)(Math.random()*10+1);
+	
+		for ( ; ; ) {
+			
+	    System.out.println(x  + " + " + y + " = ");
+	    
+	    int rezultat = MathQuiz.unos.nextInt();
+	    
+	    if (rezultat == (x +y)) {
+	    	
+	    	System.out.println("Vas odgovor je tacan");
+	    	break;
+	    	
+	    } else {
+	    	
+	    	System.out.println("Vas odgovor je pogresan, pokusajte ponovo");
+	    }
+	    
+		}
+		
+	}
+	
+	
+	
+
+	
 
 }
+
+

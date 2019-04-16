9517ee1c8ebc96e7efc4a328595a2eecdec832af
adnan hasic <hasic.adnan.ado@gmail.com>
2019-04-16 22:13:50 +0200

    Napravio metodu za mnozenje

--- src/MathematicalOperations.java
+++ src/MathematicalOperations.java
@@ -1,63 +1,77 @@
 
 public class MathematicalOperations {
-	
-	
-	
+
 	public void sabiranjeBrojeva() {
 
-		int x = (int)(Math.random()*10+1);
-		int y = (int)(Math.random()*10+1);
-	
-		for ( ; ; ) {
-			
-	    System.out.println(x  + " + " + y + " = ");
-	    
-	    int rezultat = MathQuiz.unos.nextInt();
-	    
-	    if (rezultat == (x + y)) {
-	    	
-	    	System.out.println("Vas odgovor je tacan");
-	    	break;
-	    	
-	    } else {
-	    	
-	    	System.out.println("Vas odgovor je pogresan, pokusajte ponovo");
-	    }
-	    
+		int x = (int) (Math.random() * 10 + 1);
+		int y = (int) (Math.random() * 10 + 1);
+
+		for (;;) {
+
+			System.out.println(x + " + " + y + " = ");
+
+			int rezultat = MathQuiz.unos.nextInt();
+
+			if (rezultat == (x + y)) {
+
+				System.out.println("Vas odgovor je tacan");
+				break;
+
+			} else {
+
+				System.out.println("Vas odgovor je pogresan, pokusajte ponovo");
+			}
+
 		}
-		
+
 	}
-	
+
 	public void oduzimanjeBrojeva() {
 
-		int x = (int)(Math.random()*10+1);
-		int y = (int)(Math.random()*10+1);
-	
-		for ( ; ; ) {
-			
-	    System.out.println(x  + " - " + y + " = ");
-	    
-	    int rezultat = MathQuiz.unos.nextInt();
-	    
-	    if (rezultat == (x - y)) {
-	    	
-	    	System.out.println("Vas odgovor je tacan");
-	    	break;
-	    	
-	    } else {
-	    	
-	    	System.out.println("Vas odgovor je pogresan, pokusajte ponovo");
-	    }
-	    
-		}
-		
-	}
-	
-	
-	
+		int x = (int) (Math.random() * 10 + 1);
+		int y = (int) (Math.random() * 10 + 1);
 
-	
+		for (;;) {
+
+			System.out.println(x + " - " + y + " = ");
+
+			int rezultat = MathQuiz.unos.nextInt();
+
+			if (rezultat == (x - y)) {
+
+				System.out.println("Vas odgovor je tacan");
+				break;
+
+			} else {
+
+				System.out.println("Vas odgovor je pogresan, pokusajte ponovo");
+			}
+		}
+	}
+
+	public void mnozenjeBrojeva() {
+
+		int x = (int) (Math.random() * 10 + 1);
+		int y = (int) (Math.random() * 10 + 1);
+
+		for (;;) {
+
+			System.out.println(x + " * " + y + " = ");
+
+			int rezultat = MathQuiz.unos.nextInt();
+
+			if (rezultat == (x * y)) {
+
+				System.out.println("Vas odgovor je tacan");
+				break;
+
+			} else {
+
+				System.out.println("Vas odgovor je pogresan, pokusajte ponovo");
+			}
+
+		}
+
+	}
 
 }
-
-
